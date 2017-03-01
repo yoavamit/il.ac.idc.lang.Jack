357a8cabebd40406fe855829f2d9c67f587f7142
yoav <yoav@192.168.1.121>
2017-02-28 23:01:31 -0500

    Working onthe tree outline

--- il.ac.idc.lang.Jack.ui/src/il/ac/idc/lang/ui/outline/JackOutlineTreeProvider.java
+++ il.ac.idc.lang.Jack.ui/src/il/ac/idc/lang/ui/outline/JackOutlineTreeProvider.java
@@ -3,11 +3,37 @@
 */
 package il.ac.idc.lang.ui.outline;
 
+import org.eclipse.emf.ecore.EObject;
+
+import il.ac.idc.lang.jack.ClassVarDecl;
+import il.ac.idc.lang.jack.SubroutineDecl;
+
 /**
  * Customization of the default outline structure.
  *
  * see http://www.eclipse.org/Xtext/documentation.html#outline
  */
 public class JackOutlineTreeProvider extends org.eclipse.xtext.ui.editor.outline.impl.DefaultOutlineTreeProvider {
+	
+	@Override
+	protected Object _text(Object modelElement) {
+		if (modelElement instanceof ClassVarDecl) {
+			return ((ClassVarDecl) modelElement).getAccess() + ": " + ((ClassVarDecl)modelElement).getType() + " "
+					+ ((ClassVarDecl) modelElement).getVarName();
+		}
+		if (modelElement instanceof SubroutineDecl) {
+			return ((SubroutineDecl) modelElement).getSubroutineName();
+		}
+		return super._text(modelElement);
+	}
+	
+	@Override
+	protected boolean _isLeaf(EObject modelElement) {
+		if (modelElement instanceof SubroutineDecl) {
+			return true;
+		}
+		return super._isLeaf(modelElement);
+	}
+	
 	
 }
