fc27eb10c569ed91011b404658303ecfaa0ac725
yoav <yoav@192.168.1.121>
2017-03-03 20:05:56 -0500

    added initial launch configuration type for Hack programs

---
+++ il.ac.idc.lang.Jack.ui/src/il/ac/idc/lang/ui/launch/HackLaunchConfigurationTabGroup.java
@@ -0,0 +1,17 @@
+package il.ac.idc.lang.ui.launch;
+
+import org.eclipse.debug.ui.AbstractLaunchConfigurationTabGroup;
+import org.eclipse.debug.ui.CommonTab;
+import org.eclipse.debug.ui.ILaunchConfigurationDialog;
+import org.eclipse.debug.ui.ILaunchConfigurationTab;
+
+public class HackLaunchConfigurationTabGroup extends AbstractLaunchConfigurationTabGroup {
+
+	@Override
+	public void createTabs(ILaunchConfigurationDialog dialog, String mode) {
+		ILaunchConfigurationTab[] tabs = new ILaunchConfigurationTab[] {
+				new CommonTab()
+		};
+		setTabs(tabs);
+	}
+}
